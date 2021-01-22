51c34fd8b1fa41223e99429071ad6b1188842c5c
Fabianthorsen <thorsen.fabian@gmail.com>
2021-01-22 08:58:13 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,20 +3,32 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+        const emotions = this.props.emotions.result.emotion.document.emotion;
+        const resKeys = Object.keys(
+            this.props.emotions.result.emotion.document.emotion,
+        )
+
+        return (  
+            <div>
+            {/*You can remove this line and the line below. */}
+            {JSON.stringify(this.props.emotions)}
+            <table className="table table-bordered">
+                <tbody>
+                {
+                    resKeys.keys.map((key, idx) => {
+                        return (
+                            <tr key={idx}>
+                                <td>{key}</td>
+                                <td>{emotions[key]}</td>
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </table>
+            </div>
+            );
             }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
     
 }
 export default EmotionTable;
